23b77a5c281bb510bb65ef2c4599ea67104d86dd
GoldenGarr <maxim20031128@gmail.com>
2021-12-28 13:24:55 +0300

    controller fixed

--- src/main/java/com/example/firstjavafx/HelloController.java
+++ src/main/java/com/example/firstjavafx/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.firstjavafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField login_filed;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField password_field;
+
+    @FXML
+    private Button register_button;
+
+    @FXML
+    private Button sign_button;
+
+    @FXML
+    private Label title_label;
+
+    @FXML
+    void initialize() {
+        sign_button.setOnAction(event -> {
+            System.out.println("Sign in button pressed");
+        });
     }
 }
