a2f9a91e8f571a09d46bb811046ba248bbb0738e
GoldenGarr <maxim20031128@gmail.com>
2021-12-31 21:11:19 +0300

    Sign up window added

--- src/main/java/com/example/firstjavafx/controllers/SignUpController.java
+++ src/main/java/com/example/firstjavafx/controllers/SignUpController.java
@@ -1,4 +1,33 @@
 package com.example.firstjavafx.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class SignUpController {
+    @FXML
+    private CheckBox female_check;
+
+    @FXML
+    private TextField login_filed;
+
+    @FXML
+    private CheckBox male_check;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button signup_button;
+
+    @FXML
+    private TextField signup_country;
+
+    @FXML
+    private TextField signup_name;
+
+    @FXML
+    private TextField signup_surname;
+
+    @FXML
+    private Label title_label;
 }
