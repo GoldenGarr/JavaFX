b23902cad8850374b1c3b63b9c78fcb6acdf8e97
GoldenGarr <maxim20031128@gmail.com>
2021-12-31 20:55:16 +0300

    another fxml window

--- src/main/java/com/example/firstjavafx/HelloController.java
+++ src/main/java/com/example/firstjavafx/HelloController.java
@@ -1,10 +1,16 @@
 package com.example.firstjavafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -26,6 +32,18 @@ public class HelloController {
     void initialize() {
         sign_button.setOnAction(event -> {
             System.out.println("Sign in button pressed");
+            Node node=(Node) event.getSource();
+            Stage stage=(Stage) node.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
+            Scene scene = null;
+            try {
+                scene = new Scene(fxmlLoader.load(), 700, 400);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            stage.setTitle("JavaFX app");
+            stage.setScene(scene);
+            stage.show();
         });
     }
 }
---
+++ src/main/java/com/example/firstjavafx/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.firstjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class HomeController {
+    @FXML
+    private ImageView image_frog;
+
+    @FXML
+    private Label title_label;
+}
